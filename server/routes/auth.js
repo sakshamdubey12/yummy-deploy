1faf151144b6a43f45e13ed90f908fd339c39a5f
sakshamdubey12 <87244417+sakshamdubey12@users.noreply.github.com>
2024-10-03 15:48:49 +0530

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -67,14 +67,14 @@ router.post("/login", async (req, res) => {
 
   router.get('/logout', (req, res) => {
     // Clear the token from the cookies
+    try {
     const token = req.cookies.token; // Get token from cookies
-      if (token) {
-        blacklist.add(token); // Add token to blacklist
-        res.clearCookie('token');
-      }
-    
-    // Send a response indicating the user has been logged out
+    res.clearCookie('token', { path: '/' });
     return res.status(200).json({ message: 'Successfully logged out' });
+  } catch (error) {
+    console.error('Error logging out:', error);
+    return res.status(500).json({ message: 'Internal Server Error' });
+  }
   });
 
   module.exports = router;
